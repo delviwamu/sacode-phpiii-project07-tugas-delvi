7e6267794ce947ba6d642e4c6d813beaf387204a
Delvi wamu <vilanetaicken@gmail.com>
2022-08-18 20:59:03 -0700

    if dan lese

---
+++ decision.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Logika di PHP</h1>
+    <?php
+    //======== tipe data boolean =========
+    // $hasil  = true;
+    // $hasil2 = false;
+
+    // ======== if dan else =========
+    $password  = '1234';
+    $password2 = '1234';
+
+    if($password == $password2){
+        echo 'anda berhasil masuk!';
+    }else{
+        echo 'gagal! passwordnya salah';
+    }
+
+    ?>
+    
+</body>
+</html>
